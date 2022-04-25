1ef549fa951a941e70c3562282ff5cdc4b8b41a6
zsoki <tegyukfel@gmail.com>
2022-04-25 18:11:35 +0200

    Add simple GemType attribute to gems

--- Gem/Gem.gd
+++ Gem/Gem.gd
@@ -1,2 +1,17 @@
 class_name Gem
 extends Node2D
+
+@export var gem_type_resource: Array[Resource]
+var gem_types: Array[Resource]
+var type: GemType
+
+@export_node_path(Sprite2D) var _gem_sprite
+var gem_sprite: Sprite2D
+	
+func _ready():
+	gem_sprite = get_node(_gem_sprite)
+	
+	for res in gem_type_resource: gem_types.append(res)
+	if (gem_types.size() > 0):
+		type = gem_types[randi() % gem_types.size()]
+		gem_sprite.modulate = type.color
---
+++ Gem/Type/GemType.gd
@@ -0,0 +1,4 @@
+class_name GemType
+extends Resource
+
+@export var color: Color
